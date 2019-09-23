191c05c6ca614945a6f3a821733a34b263a056a1
Dominique Skaf <doskaf@yahoo.com>
2019-09-23 23:28:59 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,9 +1,11 @@
 class Dog
   
-  attr_accessor :name
+  attr_accessor :owner
+  attr_reader :name
   
-  def initialize(name)
+  def initialize(name, owner)
     @name = name
+    @owner = owner
   end
   
 end
